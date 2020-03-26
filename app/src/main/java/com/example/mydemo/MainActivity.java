21cd5b6fda1c14ee94beea8923b8ed200cefd092
tiancong <tiancong1@zhidaoauto.com>
2020-03-26 14:35:30 +0800

    test

--- app/src/main/java/com/example/mydemo/MainActivity.java
+++ app/src/main/java/com/example/mydemo/MainActivity.java
@@ -22,11 +22,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        URI uri = URI.create("https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1584902974896&di=b57b7719e1bd7285f0096d0ceb23be3c&imgtype=0&src=http%3A%2F%2Fa4.att.hudong.com%2F21%2F09%2F01200000026352136359091694357.jpg");
-
-        ImageView imageView = findViewById(R.id.iv_qqq);
-        Glide.with(this).load(uri).into(imageView);
-
 
     }
 
